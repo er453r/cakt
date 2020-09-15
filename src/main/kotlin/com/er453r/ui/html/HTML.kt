aaeb16ac03deb803137fee74769bdde8b1c86697
Marcin Kotz <marcin.kotz@ives-system.com>
2020-09-15 16:51:30 +0200

    almost works as intended

--- src/main/kotlin/com/er453r/ui/html/HTML.kt
+++ src/main/kotlin/com/er453r/ui/html/HTML.kt
@@ -2,22 +2,26 @@ package com.er453r.ui.html
 
 import com.er453r.ui.Property
 import kotlinx.browser.document
-import org.w3c.dom.HTMLElement
+import org.w3c.dom.*
 import org.w3c.dom.events.Event
 
-fun createElement(name: String) = document.createElement(name) as HTMLElement
+fun HTMLElement.div(classes: String? = null, attributes: Map<String, String>? = null, block: HTMLDivElement.() -> Unit = {}) = element("div", classes, attributes, block)
+fun HTMLElement.label(classes: String? = null, attributes: Map<String, String>? = null, block: HTMLLabelElement.() -> Unit = {}) = element("label", classes, attributes, block)
+fun HTMLElement.input(classes: String? = null, attributes: Map<String, String>? = null, block: HTMLInputElement.() -> Unit = {}) = element("input", classes, attributes, block)
+fun HTMLElement.button(classes: String? = null, attributes: Map<String, String>? = null, block: HTMLButtonElement.() -> Unit = {}) = element("button", classes, attributes, block)
 
-fun HTMLElement.div(classes: String? = null, block: HTMLElement.() -> Unit = {}) = element("div", classes, block)
-fun HTMLElement.label(classes: String? = null, block: HTMLElement.() -> Unit = {}) = element("label", classes, block)
-fun HTMLElement.checkbox(classes: String? = null, block: HTMLElement.() -> Unit = {}) = element("checkbox", classes, block)
-fun HTMLElement.button(classes: String? = null, block: HTMLElement.() -> Unit = {}) = element("button", classes, block)
+fun HTMLElement.checkbox(classes: String? = null, attributes: Map<String, String>? = null, block: HTMLInputElement.() -> Unit = {}) = input(classes, attributes.extend(mapOf("type" to "checkbox")), block)
 
-fun HTMLElement.h1(classes: String? = null, block: HTMLElement.() -> Unit = {}) = element("h1", classes, block)
+fun Map<String, String>?.extend(extends: Map<String, String>) = (this?.toMutableMap() ?: mutableMapOf()).apply { putAll(extends) }
 
-fun HTMLElement.element(elementName: String, classes: String? = null, block: HTMLElement.() -> Unit = {}) {
-    val element = createElement(elementName)
+fun HTMLElement.h1(classes: String? = null, attributes: Map<String, String>? = null, block: HTMLHeadingElement.() -> Unit = {}) = element("h1", classes, attributes, block)
 
-    classes?.let { element.className = it }
+fun <T : HTMLElement> HTMLElement.element(elementName: String, classes: String? = null, attributes: Map<String, String>? = null, block: T.() -> Unit = {}) {
+    val element = document.createElement(elementName).unsafeCast<T>()
+
+    val attrs = classes?.let { attributes.extend(mapOf("class" to it)) } ?: attributes
+
+    attrs?.entries?.forEach { element.setAttribute(it.key, it.value) }
 
     element.apply(block)
     this.appendChild(element)
@@ -31,4 +35,4 @@ fun HTMLElement.click(block: Event.() -> Unit) {
     this.addEventListener("click", block)
 }
 
-fun HTMLElement.checkbox(property: Property<Boolean>, classes: String? = null, block: HTMLElement.() -> Unit = {}) = checkbox(classes, block)
+fun HTMLElement.checkbox(property: Property<Boolean>, classes: String? = null, attributes: Map<String, String>? = null, block: HTMLElement.() -> Unit = {}) = checkbox(classes, attributes, block)
